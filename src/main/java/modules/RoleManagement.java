e9204e99f49b13878d64d1731d66513fed21a7e0
ModdyLP <webmaster@moddylp.de>
2017-07-06 08:58:50 +0200

    06.07.2017 -- Fix Bug, Beginning RoleManager

---
+++ src/main/java/modules/RoleManagement.java
@@ -0,0 +1,13 @@
+package modules;
+
+import events.Module;
+
+/**
+ * Created by N.Hartmann on 06.07.2017.
+ * Copyright 2017
+ */
+public class RoleManagement extends Module {
+    public void addUsertoRole() {
+        
+    }
+}
