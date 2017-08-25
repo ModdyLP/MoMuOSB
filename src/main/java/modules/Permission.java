9dec5370f498bd631645d4d47e0dbf945448ba4b
ModdyLP <webmaster@moddylp.de>
2017-08-25 13:23:09 +0200

    25.08.2017 -- New Print Perm command

--- src/main/java/modules/Permission.java
+++ src/main/java/modules/Permission.java
@@ -123,31 +123,7 @@ public class Permission extends Module implements Fast {
                 if (checkIfEveroneZwei(args)) {
                     roles.add(event.getGuild().getEveryoneRole());
                 }
-                ArrayList<EmbedBuilder> builders = new ArrayList<>();
-                int page = 1;
-                EmbedBuilder builder = new EmbedBuilder();
-                builders.add(page - 1, builder);
-                builders.get(page - 1).withColor(Color.CYAN);
-                for (IRole role : roles) {
-                    ArrayList<String> permissions = PERM.getGrouppermissions().get(role);
-                    builders.get(page - 1).withTitle(String.format(LANG.getTranslation("permlist_title"),
-                            role.getName()));
-                    for (String permission: permissions) {
-                        for (Command command: COMMAND.getCommandByPermission(permission)) {
-                            builders.get(page - 1).appendField(LANG.getTranslation("help_command")+
-                                    ": "+command.command(),
-                                    LANG.getTranslation("help_permission")+": "+permission, false);
-                            if (builders.get(page - 1).getFieldCount() == EmbedBuilder.FIELD_COUNT_LIMIT) {
-                                EmbedBuilder buildertemp = new EmbedBuilder();
-                                builders.add(page - 1, buildertemp);
-                                page++;
-                            }
-                        }
-                    }
-                }
-                for (EmbedBuilder builderinst: builders) {
-                    BotUtils.sendPrivEmbMessage(event.getAuthor().getOrCreatePMChannel(), builderinst, false);
-                }
+                buildMessage(roles, event);
             } else {
                 BotUtils.sendEmbMessage(event.getChannel(), SMB.shortMessage(LANG.getTranslation("norolefound")), true);
             }
@@ -158,9 +134,35 @@ public class Permission extends Module implements Fast {
 
     }
 
+    @Command(
+            command = "printperm",
+            arguments = {"ServerID"},
+            description = "Print Perm for specific Server",
+            permission = Globals.BOT_PERM,
+            prefix = Globals.ADMIN_PREFIX,
+            alias = "printp"
+    )
+    public void printoutgroups(MessageReceivedEvent event, String[] args) {
+        try {
+            List<IRole> roles = INIT.BOT.getGuildByID(Long.valueOf(args[0])).getRoles();
+            if (roles.size() > 0) {
+                roles.add(INIT.BOT.getGuildByID(Long.valueOf(args[0])).getEveryoneRole());
+                buildMessage(roles, event);
+            } else {
+                BotUtils.sendEmbMessage(event.getChannel(), SMB.shortMessage(LANG.getTranslation("norolefound")), true);
+            }
+
+        } catch (Exception ex) {
+            Console.error("Error on adding Permission" + ex.getMessage());
+            Console.error(ex);
+        }
+
+    }
+
     public boolean checkIfEverone(String[] args) {
         return Utils.makeArgsToString(args, new String[]{args[0]}).equalsIgnoreCase("everyone");
     }
+
     public boolean checkIfEveroneZwei(String[] args) {
         return Utils.makeArgsToString(args, new String[]{}).equalsIgnoreCase("everyone");
     }
@@ -177,5 +179,33 @@ public class Permission extends Module implements Fast {
         DRIVER.setProperty(DEF_LANG, "perm_rem_failed", "Failed to remove Permission to group.");
         DRIVER.setProperty(DEF_LANG, "permlist_title", "Permission List for Group %1s");
         DRIVER.setProperty(DEF_LANG, "norolefound", "The Role was not found.");
+    }
+
+    private void buildMessage(List<IRole> roles, MessageReceivedEvent event) {
+        ArrayList<EmbedBuilder> builders = new ArrayList<>();
+        int page = 1;
+        EmbedBuilder builder = new EmbedBuilder();
+        builders.add(page - 1, builder);
+        builders.get(page - 1).withColor(Color.CYAN);
+        for (IRole role : roles) {
+            ArrayList<String> permissions = PERM.getGrouppermissions().get(role);
+            builders.get(page - 1).withTitle(String.format(LANG.getTranslation("permlist_title"),
+                    role.getName()+"   "+role.getGuild().getName()));
+            for (String permission : permissions) {
+                for (Command command : COMMAND.getCommandByPermission(permission)) {
+                    builders.get(page - 1).appendField(LANG.getTranslation("help_command") +
+                                    ": " + command.command(),
+                            LANG.getTranslation("help_permission") + ": " + permission, false);
+                    if (builders.get(page - 1).getFieldCount() == EmbedBuilder.FIELD_COUNT_LIMIT) {
+                        EmbedBuilder buildertemp = new EmbedBuilder();
+                        builders.add(page - 1, buildertemp);
+                        page++;
+                    }
+                }
+            }
+        }
+        for (EmbedBuilder builderinst : builders) {
+            BotUtils.sendPrivEmbMessage(event.getAuthor().getOrCreatePMChannel(), builderinst, false);
+        }
     }
 }
